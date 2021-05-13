5049577cf3524e0ba75f3f8284923139f7960eba
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 09:49:28 -0400

    Update rust-cardano chain-core  src

---
+++ rust-cardano/chain-core/src/lib.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate cfg_if;
+
+cfg_if! {
+    if #[cfg(test)] {
+        extern crate quickcheck;
+    } else if #[cfg(feature = "property-test-api")] {
+        extern crate quickcheck;
+    }
+}
+
+pub mod mempack;
+pub mod packer;
+pub mod property;
