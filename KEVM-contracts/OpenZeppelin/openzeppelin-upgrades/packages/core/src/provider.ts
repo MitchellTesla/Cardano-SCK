bcce4a52d910e1692e56dfd622bcc5f4d235cd96
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 11:59:45 -0400

    Add files via upload

---
+++ KEVM-contracts/OpenZeppelin/openzeppelin-upgrades/packages/core/src/provider.ts
@@ -0,0 +1,77 @@
+import BN from 'bn.js';
+
+export interface EthereumProvider {
+  send(method: 'web3_clientVersion', params: []): Promise<string>;
+  send(method: 'net_version', params: []): Promise<string>;
+  send(method: 'eth_chainId', params: []): Promise<string>;
+  send(method: 'eth_getCode', params: [string, string]): Promise<string>;
+  send(method: 'eth_getStorageAt', params: [string, string, string]): Promise<string>;
+  send(method: 'eth_getTransactionByHash', params: [string]): Promise<null | EthereumTransaction>;
+  send(method: string, params: unknown[]): Promise<unknown>;
+}
+
+interface EthereumTransaction {
+  blockHash: string | null;
+}
+
+export async function getNetworkId(provider: EthereumProvider): Promise<string> {
+  return provider.send('net_version', []);
+}
+
+export async function getChainId(provider: EthereumProvider): Promise<number> {
+  const id = await provider.send('eth_chainId', []);
+  return new BN(id.replace(/^0x/, ''), 'hex').toNumber();
+}
+
+export async function getClientVersion(provider: EthereumProvider): Promise<string> {
+  return provider.send('web3_clientVersion', []);
+}
+
+export async function getStorageAt(
+  provider: EthereumProvider,
+  address: string,
+  position: string,
+  block = 'latest',
+): Promise<string> {
+  const storage = await provider.send('eth_getStorageAt', [address, position, block]);
+  const padded = storage.replace(/^0x/, '').padStart(64, '0');
+  return '0x' + padded;
+}
+
+export async function getCode(provider: EthereumProvider, address: string, block = 'latest'): Promise<string> {
+  return provider.send('eth_getCode', [address, block]);
+}
+
+export async function hasCode(provider: EthereumProvider, address: string, block?: string): Promise<boolean> {
+  const code = await getCode(provider, address, block);
+  return code !== '0x';
+}
+
+export async function getTransactionByHash(
+  provider: EthereumProvider,
+  txHash: string,
+): Promise<EthereumTransaction | null> {
+  return provider.send('eth_getTransactionByHash', [txHash]);
+}
+
+export const networkNames: { [chainId in number]?: string } = Object.freeze({
+  1: 'mainnet',
+  2: 'morden',
+  3: 'ropsten',
+  4: 'rinkeby',
+  5: 'goerli',
+  42: 'kovan',
+});
+
+export async function isDevelopmentNetwork(provider: EthereumProvider): Promise<boolean> {
+  const chainId = await getChainId(provider);
+  //  1337 => ganache and geth --dev
+  // 31337 => hardhat network
+  if (chainId === 1337 || chainId === 31337) {
+    return true;
+  } else {
+    const clientVersion = await getClientVersion(provider);
+    const [name] = clientVersion.split('/', 1);
+    return name === 'HardhatNetwork' || name === 'EthereumJS TestRPC';
+  }
+}
