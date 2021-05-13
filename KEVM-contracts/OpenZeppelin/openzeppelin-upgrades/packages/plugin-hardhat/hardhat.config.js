d510da4c5b0a71ffcddb9b413f33f952d3c0b312
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 11:54:41 -0400

    Add files via upload

---
+++ KEVM-contracts/OpenZeppelin/openzeppelin-upgrades/packages/plugin-hardhat/hardhat.config.js
@@ -0,0 +1,14 @@
+require('@openzeppelin/hardhat-upgrades');
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.5.15',
+      },
+      {
+        version: '0.6.7',
+      },
+    ],
+  },
+};
