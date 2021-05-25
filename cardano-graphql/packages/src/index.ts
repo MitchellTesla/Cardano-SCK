f74d6903c65f6b3de3445f3ffe8202c5386d4a2e
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-24 23:43:51 -0400

    🔓🔑 update cardano-graphql/packages/src

---
+++ cardano-graphql/packages/src/index.ts
@@ -0,0 +1,12 @@
+import { onFailedAttemptFor } from './onFailedAttemptFor'
+import { loadQueryNode } from './queryNodeLoading'
+import * as scalars from './scalars'
+export * from './batching'
+export * from './data_fetching'
+export * from './stringModifiers'
+
+export default {
+  onFailedAttemptFor,
+  loadQueryNode,
+  scalars
+}
---
+++ cardano-graphql/packages/src/onFailedAttemptFor.ts
@@ -0,0 +1,12 @@
+import { FailedAttemptError } from 'p-retry'
+import { dummyLogger, Logger } from 'ts-log'
+
+export const onFailedAttemptFor = (operation: string, logger: Logger = dummyLogger) => ({ attemptNumber, message, retriesLeft }: FailedAttemptError) => {
+  const nextAction = retriesLeft > 0 ? 'retrying...' : 'exiting'
+  logger.trace(message)
+  logger.debug(`${operation}: Attempt ${attemptNumber} of ${attemptNumber + retriesLeft}, ${nextAction}`)
+  if (retriesLeft === 0) {
+    logger.error(message)
+    process.exit(1)
+  }
+}
