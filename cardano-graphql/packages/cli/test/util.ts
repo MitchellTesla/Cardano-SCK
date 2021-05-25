b210fee5f68d6976a6831f7cfce34a71bbb14db7
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-25 00:09:04 -0400

    🧪 Update cardano-graphql/packages/cli/test

---
+++ cardano-graphql/packages/cli/test/util.ts
@@ -0,0 +1,32 @@
+import { spawn } from 'child_process'
+import DoneCallback = jest.DoneCallback
+
+export function setup (done: DoneCallback) {
+  teardown()
+  setTimeout(() => {
+    const yarnAddGlobal = spawn('yarn', ['global:add'])
+    yarnAddGlobal.stderr.on('error', (error) => done(error))
+    yarnAddGlobal.on('close', done)
+  }, 6000)
+}
+
+export function teardown (done?: DoneCallback) {
+  const yarnRemoveGlobal = spawn('yarn', ['global:remove'])
+  yarnRemoveGlobal.stderr.on('error', (error) => {
+    if (done !== undefined) done(error)
+  })
+  yarnRemoveGlobal.on('close', () => {
+    if (done !== undefined) done()
+  })
+}
+
+export function dockerCommandCleanup (done: DoneCallback) {
+  const cgqlCleanup = spawn('cgql', ['docker', 'cleanup', '-f'])
+  cgqlCleanup.stderr.on('error', (error) => {
+    console.error(error)
+    if (error) return done(error)
+  })
+  cgqlCleanup.on('close', () => {
+    done()
+  })
+}
