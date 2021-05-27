9789207e9b12ba9bc2d9b97a08bd453f64d842bd
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-26 22:59:39 -0400

    ♻️ update daedalus/static/4.0.5 🔃 source-renderer * app * network requests

---
+++ static/daedalus/static/4.0.5/source/renderer/app/network/requests/getNetworkParameters.js
@@ -0,0 +1,13 @@
+// @flow
+import type { RequestConfig } from '../../common/types';
+import type { GetNetworkParametersApiResponse } from '../types';
+import { request } from '../../utils/request';
+
+export const getNetworkParameters = (
+  config: RequestConfig
+): Promise<GetNetworkParametersApiResponse> =>
+  request({
+    method: 'GET',
+    path: '/v2/network/parameters',
+    ...config,
+  });
