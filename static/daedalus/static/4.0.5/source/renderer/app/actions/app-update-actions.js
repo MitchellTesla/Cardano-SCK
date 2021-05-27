716d847e5e465fd0d98ce55f85037c75559d23ad
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-26 22:52:51 -0400

    ♻️ update daedalus/static/4.0.5 🔃 source/renderer/app-actions

---
+++ static/daedalus/static/4.0.5/source/renderer/app/actions/app-update-actions.js
@@ -0,0 +1,11 @@
+// @flow
+import Action from './lib/Action';
+
+// ======= APP UPDATE ACTIONS =======
+
+export default class AppUpdateActions {
+  installUpdate: Action<any> = new Action();
+  postponeUpdate: Action<any> = new Action();
+  openAppUpdateOverlay: Action<any> = new Action();
+  closeAppUpdateOverlay: Action<any> = new Action();
+}
