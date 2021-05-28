5f7516f05371b60a2dd2767fef2f58c936c74971
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-28 03:33:47 -0400

    ⚙ Update 🔄 Hydra* hydra-poc * hydra-node/test 🔬 Hydra

---
+++ hydra/hydra-poc/hydra-node/test/Hydra/MockZMQChainSpec.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+
+module Hydra.MockZMQChainSpec where
+
+import Cardano.Prelude
+import qualified Data.Set as Set
+import Data.String (String)
+import Hydra.Ledger (ParticipationToken (..))
+import Hydra.Logic (OnChainTx (InitTx))
+import Hydra.MockZMQChain (catchUpTransactions, mockChainClient, runChainSync, startChain)
+import Hydra.Logging (nullTracer)
+import System.Timeout (timeout)
+import Test.Hspec (Spec, describe, it, shouldReturn)
+
+spec :: Spec
+spec =
+  describe "Mock 0MQ-Based Chain" $ do
+    let tx = InitTx (Set.fromList [ParticipationToken 2 1, ParticipationToken 2 2])
+        numberOfTxs :: Int
+        numberOfTxs = 3
+
+    it "publish transactions received from a client given chain is started" $ do
+      withMockZMQChain 54321 54322 54323 $ \syncAddress _catchUpAddress postAddress -> do
+        mvar <- newEmptyMVar
+        void $
+          concurrently
+            ( -- we lack proper synchronisation so better give chain sync time to join the party
+              threadDelay 500_000 >> mockChainClient postAddress nullTracer tx
+            )
+            (within3second $ runChainSync syncAddress (putMVar mvar) nullTracer)
+
+        within3second (takeMVar mvar) `shouldReturn` Just tx
+
+    it "catches up transacions with mock chain" $ do
+      chan <- newChan
+      withMockZMQChain 54324 54325 54326 $ \_syncAddress catchUpAddress postAddress -> do
+        forM_ [1 .. numberOfTxs] $ const $ mockChainClient postAddress nullTracer tx
+        catchUpTransactions catchUpAddress (writeChan chan) nullTracer
+        within3second (forM [1 .. numberOfTxs] (const $ readChan chan)) `shouldReturn` Just [tx, tx, tx]
+
+withMockZMQChain :: Int -> Int -> Int -> (String -> String -> String -> IO ()) -> IO ()
+withMockZMQChain syncPort catchUpPort postPort action =
+  withAsync (startChain syncAddress catchUpAddress postAddress nullTracer) $ \_ -> do
+    action syncAddress catchUpAddress postAddress
+ where
+  syncAddress = "tcp://127.0.0.1:" <> show syncPort
+  catchUpAddress = "tcp://127.0.0.1:" <> show catchUpPort
+  postAddress = "tcp://127.0.0.1:" <> show postPort
+
+within3second :: IO a -> IO (Maybe a)
+within3second = timeout 3_000_000
---
+++ hydra/hydra-poc/hydra-node/test/Hydra/NetworkSpec.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+-- | Test the real networking layer
+module Hydra.NetworkSpec where
+
+import Cardano.Prelude hiding (threadDelay)
+
+import Cardano.Binary (FromCBOR, ToCBOR, fromCBOR, toCBOR)
+import Codec.CBOR.Read (deserialiseFromBytes)
+import Codec.CBOR.Write (toLazyByteString)
+import Codec.Serialise (Serialise, deserialiseOrFail, serialise)
+import Control.Monad.Class.MonadTimer (threadDelay, timeout)
+import Hydra.Logic (HydraMessage (..))
+import Hydra.Network.Ouroboros (broadcast, withOuroborosHydraNetwork)
+import Hydra.Network.ZeroMQ (withZeroMQHydraNetwork)
+import Hydra.Logging (nullTracer)
+import Test.Hspec (Spec, describe, expectationFailure, it, shouldReturn)
+import Test.QuickCheck (Arbitrary (..), arbitrary, oneof, property)
+
+type MockTx = ()
+
+spec :: Spec
+spec = describe "Networking layer" $ do
+  let requestTx :: HydraMessage Integer
+      requestTx = ReqTx 1
+
+      lo = "127.0.0.1"
+
+  describe "Ouroboros Network" $
+    it "broadcasts messages to single connected peer" $ do
+      received <- newEmptyMVar
+      failAfter2Seconds $ do
+        withOuroborosHydraNetwork (lo, "45678") [(lo, "45679")] (const $ pure ()) $ \hn1 ->
+          withOuroborosHydraNetwork (lo, "45679") [(lo, "45678")] (putMVar received) $ \_ -> do
+            broadcast hn1 requestTx
+            takeMVar received `shouldReturn` requestTx
+
+  describe "0MQ Network" $
+    it "broadcasts messages to 2 connected peers" $ do
+      node2received <- newEmptyMVar
+      node3received <- newEmptyMVar
+      failAfter2Seconds $ do
+        withZeroMQHydraNetwork (lo, "55677") [(lo, "55678"), (lo, "55679")] nullTracer (const $ pure ()) $ \hn1 ->
+          withZeroMQHydraNetwork (lo, "55678") [(lo, "55677"), (lo, "55679")] nullTracer (putMVar node2received) $ \_ ->
+            withZeroMQHydraNetwork (lo, "55679") [(lo, "55677"), (lo, "55678")] nullTracer (putMVar node3received) $ \_ -> do
+              threadDelay 1 -- This is needed to wait for all nodes to be up
+              broadcast hn1 requestTx
+              takeMVar node2received `shouldReturn` requestTx
+              takeMVar node3received `shouldReturn` requestTx
+
+  describe "Serialisation" $ do
+    it "can roundtrip serialisation of HydraMessage" $ property $ prop_canRoundtripSerialise @(HydraMessage Integer)
+    it "can roundtrip CBOR encoding/decoding of HydraMessage" $ property $ prop_canRoundtripCBOREncoding @(HydraMessage Integer)
+
+instance Arbitrary (HydraMessage Integer) where
+  arbitrary = oneof [ReqTx <$> arbitrary, pure AckTx, pure ConfTx, pure ReqSn, pure AckSn, pure ConfSn]
+
+prop_canRoundtripSerialise :: (Serialise a, Eq a) => a -> Bool
+prop_canRoundtripSerialise a =
+  let encoded = serialise a
+   in deserialiseOrFail encoded == Right a
+
+prop_canRoundtripCBOREncoding ::
+  (ToCBOR a, FromCBOR a, Eq a) => a -> Bool
+prop_canRoundtripCBOREncoding a =
+  let encoded = toLazyByteString $ toCBOR a
+   in (snd <$> deserialiseFromBytes fromCBOR encoded) == Right a
+
+failAfter2Seconds :: IO () -> IO ()
+failAfter2Seconds action =
+  timeout 2 action >>= \case
+    Nothing -> expectationFailure "Test timed out after 2 seconds"
+    Just _ -> pure ()
