e07d8bf082f42a271d3c305b21c9eaa42ded51a5
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-29 23:20:41 -0400

    feat: adding pool retirement operations

---
+++ pkg/cardano-rosetta/cardano-rosetta-server/src/server/utils/validations.ts
@@ -0,0 +1,37 @@
+import { PUBLIC_KEY_BYTES_LENGTH, ADA, AddressType, CurveType, ASSET_NAME_LENGTH, POLICY_ID_LENGTH } from './constants';
+import { ErrorFactory } from './errors';
+import { isEmptyHexString } from './formatters';
+import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
+
+const tokenNameValidation = new RegExp(`^[0-9a-fA-F]{0,${ASSET_NAME_LENGTH}}$`);
+
+const policyIdValidation = new RegExp(`^[0-9a-fA-F]{${POLICY_ID_LENGTH}}$`);
+
+export const VALID_CURVE_TYPE = CurveType.edwards25519;
+
+export const isKeyValid = (publicKeyBytes: string, curveType: string): boolean =>
+  publicKeyBytes.length === PUBLIC_KEY_BYTES_LENGTH && curveType === CurveType.edwards25519;
+
+export const isAddressTypeValid = (type: string): boolean =>
+  [...Object.values(AddressType), '', undefined].includes(type);
+
+export const isTokenNameValid = (name: string): boolean => tokenNameValidation.test(name) || isEmptyHexString(name);
+
+export const isPolicyIdValid = (policyId: string): boolean => policyIdValidation.test(policyId);
+
+export const validateCurrencies = (currencies: Components.Schemas.Currency[]): void => {
+  currencies.forEach(({ symbol, metadata }) => {
+    if (!isTokenNameValid(symbol)) throw ErrorFactory.invalidTokenNameError(`Given name is ${symbol}`);
+    if (symbol !== ADA && !isPolicyIdValid(metadata.policyId))
+      throw ErrorFactory.invalidPolicyIdError(`Given policy id is ${metadata.policyId}`);
+  });
+};
+export const isEd25519KeyHash = (hash: string): boolean => {
+  let edd25519Hash: CardanoWasm.Ed25519KeyHash;
+  try {
+    edd25519Hash = CardanoWasm.Ed25519KeyHash.from_bytes(Buffer.from(hash, 'hex'));
+  } catch (error) {
+    return false;
+  }
+  return !!edd25519Hash;
+};
