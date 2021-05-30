31bbcbbd4e17132989b8a9fca6cc48a2a7d48457
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-29 23:34:16 -0400

    Feat/add pool registrations at construction api

---
+++ pkg/cardano-rosetta/cardano-rosetta-server/src/server/services/services.ts
@@ -0,0 +1,37 @@
+import { Repositories } from '../db/repositories';
+import blockService, { BlockService } from './block-service';
+import cardanoService, { CardanoService, LinearFeeParameters, DepositParameters } from './cardano-services';
+import constructionService, { ConstructionService } from './construction-service';
+import networkService, { NetworkService, TopologyConfig } from './network-service';
+
+export interface Services {
+  blockService: BlockService;
+  constructionService: ConstructionService;
+  networkService: NetworkService;
+  cardanoService: CardanoService;
+}
+
+/**
+ * Configures all the services required by the app
+ *
+ * @param repositories repositories to be used by the services
+ */
+export const configure = (
+  repositories: Repositories,
+  networkId: string,
+  networkMagic: number,
+  topologyFile: TopologyConfig,
+  DEFAULT_RELATIVE_TTL: number,
+  depositParameters: DepositParameters
+  // FIXME: we can group networkId and networkMagic in a new type
+  // eslint-disable-next-line max-params
+): Services => {
+  const cardanoServiceInstance = cardanoService(depositParameters);
+  const blockServiceInstance = blockService(repositories.blockchainRepository, cardanoServiceInstance);
+  return {
+    blockService: blockServiceInstance,
+    constructionService: constructionService(blockServiceInstance, DEFAULT_RELATIVE_TTL),
+    networkService: networkService(networkId, networkMagic, blockServiceInstance, topologyFile),
+    cardanoService: cardanoServiceInstance
+  };
+};
