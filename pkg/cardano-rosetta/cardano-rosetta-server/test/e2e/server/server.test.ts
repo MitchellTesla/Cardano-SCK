ef27bf45cbe6cc66c6d8501c2760aa77e66906ea
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-29 23:41:10 -0400

    feat: multiple snapshots and fixtures are now allowed when running tests

---
+++ pkg/cardano-rosetta/cardano-rosetta-server/test/e2e/server/server.test.ts
@@ -0,0 +1,58 @@
+/* eslint-disable no-magic-numbers */
+import { FastifyInstance } from 'fastify';
+import StatusCodes from 'http-status-codes';
+import { Pool } from 'pg';
+import { setupDatabase, setupServer } from '../utils/test-utils';
+import { generateNetworkPayload } from '../network/common';
+import { CARDANO, MAINNET } from '../../../src/server/utils/constants';
+
+const genericErrorMatcher = (regexp: RegExp) =>
+  expect.objectContaining({
+    code: 5000,
+    details: {
+      message: expect.stringMatching(
+        // eslint-disable-next-line prettier/prettier
+        regexp
+      )
+    },
+    message: 'An error occurred',
+    retriable: true
+  });
+
+describe('Server test', () => {
+  let database: Pool;
+  let server: FastifyInstance;
+  beforeAll(async () => {
+    database = setupDatabase();
+    server = setupServer(database);
+  });
+
+  afterAll(async () => {
+    await database.end();
+  });
+
+  test('should return a generic error if payload is not valid', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: '/block',
+      payload: { asdasa: 10 }
+    });
+
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual(
+      genericErrorMatcher(/An error occurred for request \d: body should have required property 'network_identifier'/)
+    );
+  });
+
+  test('should return a generic error if there is db connection problem', async () => {
+    await database.end();
+    const response = await server.inject({
+      method: 'post',
+      url: '/network/status',
+      payload: generateNetworkPayload(CARDANO, MAINNET)
+    });
+
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual(genericErrorMatcher(/An error occurred for request.*/));
+  });
+});
